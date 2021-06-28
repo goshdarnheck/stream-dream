4f08ff416cf11ee8ece0e8c7cbbc4165d7bd3c85
Matthew Rapati <mattrapati@gmail.com>
2021-06-28 19:52:53 -0400

    Store and try to reuse refresh token. Open Spotify Auth log when needed. Removed music note from song name.

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function pollCurrentTrack(delay) {
       .then(function(data) {
         if (data.body && data.body.item) {
           console.log('> Now playing: ', data.body.item.name);
-          win.webContents.send('currentTrack', data)
+          win.webContents.send('currentTrack', data);
           console.log("| Next poll in:", data.body.item.duration_ms - data.body.progress_ms + 1000)
           pollId = setTimeout(getCurrentTrack, data.body.item.duration_ms - data.body.progress_ms + 1000);
         }
@@ -50,31 +50,35 @@ function refreshSpotifyToken(callback) {
 
       // Save the access token so that it's used in future calls
       spotifyApi.setAccessToken(data.body['access_token']);
+      store.set('access_token', data.body['access_token']);
+      if (data.body['refresh_token']) {
+        store.set('refresh_token', data.body['refresh_token']);
+      }
 
       // call that callback // code comment // todo: clean up comments
       callback()
     },
     function(err) {
       console.log('> Could not refresh access token', err);
+      // TODO: GET ERROR, probably need to get new auth code
+      openSpotifyAuthWindow()
     }
   );
 }
 
 function getSpotifyAccessAndRefreshTokens(code, callback) {
-  // Retrieve an access token and a refresh token
-  let tokenExpirationEpoch;
   spotifyApi.authorizationCodeGrant(code).then(
     function(data) {
-      console.log('The token expires in ' + data.body['expires_in']);
-      console.log('The access token is ' + data.body['access_token']);
-      console.log('The refresh token is ' + data.body['refresh_token']);
+      console.log('> The token expires in ' + data.body['expires_in']);
 
       // Set the access token on the API object to use it in later calls
       spotifyApi.setAccessToken(data.body['access_token']);
       spotifyApi.setRefreshToken(data.body['refresh_token']);
+      store.set('access_token', data.body['access_token']);
+      store.set('refresh_token', data.body['refresh_token']);
 
       // Save the amount of seconds until the access token expired
-      tokenExpirationEpoch =
+      const tokenExpirationEpoch =
         new Date().getTime() / 1000 + data.body['expires_in'];
       console.log(
         'Retrieved token. It expires in ' +
@@ -86,6 +90,10 @@ function getSpotifyAccessAndRefreshTokens(code, callback) {
     },
     function(err) {
       console.log('Something went wrong!', err);
+
+      if (err.body.error === 'invalid_grant') {
+        openSpotifyAuthWindow()
+      }
     }
   );
 }
@@ -131,7 +139,7 @@ function handleSpotifyAuthResponseUrl(callBackUrl) {
   const spotifyAuthCode = callBackUrl.searchParams.get('code')
   if (spotifyAuthCode) {
     console.log(`> Spotify auth code: ${spotifyAuthCode}`)
-    store.set('spotifyAuthCode', spotifyAuthCode);
+
     getSpotifyAccessAndRefreshTokens(spotifyAuthCode, function () {
       pollCurrentTrack(0)
       authWindow.close();
@@ -159,44 +167,52 @@ app.on('ready', function () {
     fullscreenable: false,
     resizable: false,
     paintWhenInitiallyHidden: true,
-    disableHtmlFullscreenWindowResize: true
+    disableHtmlFullscreenWindowResize: true,
+    show: false
   });
 
   win.setAlwaysOnTop(true, 'screen');
   win.loadURL(`file://${__dirname}/main.html`)
+  win.webContents.on('did-finish-load', function() {
+    win.show();
 
-  authWindow = new BrowserWindow({
-    width: 800, 
-    height: 600, 
-    show: false, 
-    'node-integration': false
-  });
-
-  if (displaySpotifySong) {
-    openSpotifyAuthWindow()
-  }
-
-  // console.log(store.get('spotifyAuthCode'))
-  // if spotifyAuthCode in store then
-    // try to call getSpotifyAccessAndRefreshTokens with a callback to start polling
-    // catch error, openSpotifyAuthWindow with callback to start polling 
-
-  session.defaultSession.webRequest.onBeforeRequest(
-    {
-      urls: [config.spotify.callbackUri + '*']
-    },
-    (details, callback) => {
-      const callBackUrl = new URL(details.url);
-
-      if (callBackUrl.pathname === '/spotifyAuthCodeCallback') {
-        handleSpotifyAuthResponseUrl(callBackUrl)
+    authWindow = new BrowserWindow({
+      width: 800, 
+      height: 600, 
+      show: false, 
+      'node-integration': false
+    });
+  
+    if (displaySpotifySong) {
+      if (store.get('access_token') && store.get('refresh_token')) {
+        spotifyApi.setAccessToken(store.get('access_token'));
+        spotifyApi.setRefreshToken(store.get('refresh_token'));
+  
+        refreshSpotifyToken(function () {
+          pollCurrentTrack(0)
+        });
+      } else {
+        openSpotifyAuthWindow();
       }
-
-      callback({
-        cancel: false
-      });
     }
-  );
+  
+    session.defaultSession.webRequest.onBeforeRequest(
+      {
+        urls: [config.spotify.callbackUri + '*']
+      },
+      (details, callback) => {
+        const callBackUrl = new URL(details.url);
+  
+        if (callBackUrl.pathname === '/spotifyAuthCodeCallback') {
+          handleSpotifyAuthResponseUrl(callBackUrl)
+        }
+  
+        callback({
+          cancel: false
+        });
+      }
+    );
+  });
 });
 
 app.on('before-quit', () => {
